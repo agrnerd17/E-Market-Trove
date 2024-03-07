63075d96c76f0badac581ce0c51e1d77fcbaf43a
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2024-03-06 17:00:51 -0800

    Add files via upload

--- e-market-trove/src/App.js
+++ e-market-trove/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // Import react for front end
+import './App.css'; // Import in style settings
 
-function App() {
+import logo from './logo.svg'; // Import placeholder logo until we can make our own
+
+// Main website function
+function EMarketTrove() { 
+  React.useEffect(() => { // allow react gui elemeents 
+    document.title = "E-Market Trove"; }, []); // set title for tab
+
+  // Return output to display
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App">    
+      <h1>E-Market Trove</h1> {/* set title for website*/}
+      <header className="App-header">   
+        <img src={logo} className="App-logo" alt="logo" /> {/* set our logo */} 
+        <p> 
+          Just testing things -Victor {/* space reversed for text */}
+          This is just placeholder code until we design a better foundation.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default EMarketTrove; // alows other files read App.js function
