3d6347239b9d5927d43fde7fd4a14933f714d16c
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2024-03-27 14:30:25 -0700

    more testing

--- e-market-trove/src/App.js
+++ e-market-trove/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'; // Import react for front end
 import './App.css'; // Import in style settings
-
+import React from 'react'; // Import react for front end
 import logo from './logo.svg'; // Import placeholder logo until we can make our own
 
 // Main website function
-function EMarketTrove() { 
+function EMarketTrove() {
   React.useEffect(() => { // allow react gui elemeents 
-    document.title = "E-Market Trove"; }, []); // set title for tab
+    document.title = "E-Market Trove";
+  }, []); // set title for tab
 
   // Return output to display
   return (
-    <div className="App">    
+    <div className="App">
       <h1>E-Market Trove</h1> {/* set title for website*/}
-      <header className="App-header">   
-        <img src={logo} className="App-logo" alt="logo" /> {/* set our logo */} 
-        <p> 
-          Just testing things -Victor {/* space reversed for text */}
-          This is just placeholder code until we design a better foundation.
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Just testing things. - Victor <br /> {/* break for a new line */}
+          This is placeholder code until we design a better foundation.
         </p>
       </header>
     </div>
